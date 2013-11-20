a8c7184b4f5b198716f637b1a4e12f8498666818
Jonathan D. Baker <jbaker@nexgenstorage.com>
2013-11-19 17:22:35 -0700

    Define initialize and validate method of Bill model.

--- js/app/models.js
+++ js/app/models.js
@@ -1,7 +1,40 @@
 TipTip.module(
         "Models", function(Models, TipTip, Backbone, Marionette, $, _) {
     
-    Models.Bill = Backbone.Model.extend({});
+    Models.Bill = Backbone.Model.extend({
+
+        // Recalculate 'tipAmount' whenever 'amount' changes
+        initialize: function() {
+            this.on("change", function() {
+                if (this.changed.hasOwnProperty("amount")) {
+                    var amount = parseFloat(
+                        this.changed.amount.replace(",", ""));
+                    var tip = amount * this.get("tipPercentage") / 100.0;
+                    this.set({
+                        tipAmount: (Math.ceil(tip * 100) / 100).toFixed(2)});
+                }
+            });
+        },
+
+        // Validate 'amount' attribute
+        validate: function(attrs) {
+            var val = attrs.amount,
+                valid = true;
+
+            if (val.length === 0) {
+                valid = false;
+            } else if (val.length === 1) {
+                valid = (val.search(/^\d$/) >= 0);
+            } else {
+                valid = (val.search(/^\.([\d]{1,2})$/) >= 0 ||
+                        val.search(/^\d+(,\d{3})*(\.\d{1,2})?$/) >= 0);
+            }
+
+            if (!valid) {
+                return "ERROR";
+            }
+        }
+    });
 
     Models.Bills = Backbone.Collection.extend({
         model: Models.Bill
